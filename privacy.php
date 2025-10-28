ebbcd847b39829f3a52b364480fb654b51004cc6
vishwajeet <vk5185772@gmail.com>
2025-10-28 12:22:03 +0530

    footer

--- privacy.php
+++ privacy.php
@@ -99,45 +99,8 @@ include 'includes/navbar.php';
         </div>
     </div>
     
-    <footer class="footer mt-5">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6">
-                    <h5>KamateRaho</h5>
-                    <p>India's best cashback and coupons website. Save money on every purchase.</p>
-                </div>
-                <div class="col-md-3">
-                    <h5>Quick Links</h5>
-                    <ul class="list-unstyled">
-                        <li><a href="index.php" class="text-white">Home</a></li>
-                        <li><a href="index.php#categories" class="text-white">Categories</a></li>
-                        <li><a href="about.php" class="text-white">About Us</a></li>
-                        <li><a href="contact.php" class="text-white">Contact</a></li>
-                        <?php if (isset($_SESSION['user_id'])): ?>
-                            <li><a href="dashboard.php" class="text-white">Dashboard</a></li>
-                        <?php else: ?>
-                            <li><a href="login.php" class="text-white">Login</a></li>
-                            <li><a href="register.php" class="text-white">Register</a></li>
-                        <?php endif; ?>
-                    </ul>
-                </div>
-                <div class="col-md-3">
-                    <h5>Legal</h5>
-                    <ul class="list-unstyled">
-                        <li><a href="terms.php" class="text-white">Terms & Conditions</a></li>
-                        <li><a href="privacy.php" class="text-white">Privacy Policy</a></li>
-                    </ul>
-                    <h5>Contact Us</h5>
-                    <p>Email: support@kamateraho.com</p>
-                    <p>Phone: +91 9876543210</p>
-                </div>
-            </div>
-            <hr class="bg-light">
-            <div class="text-center">
-                <p>&copy; 2025 KamateRaho. All rights reserved.</p>
-            </div>
-        </div>
-    </footer>
+  <!-- Footer -->
+   <?php include 'includes/footer.php'; ?>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
